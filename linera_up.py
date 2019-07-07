6f1c6d8e5d63589d59f35914ccb65467e2a21fd3
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2019-07-08 00:15:33 +0530

    Update linera_up.py

--- linera_up.py
+++ linera_up.py
@@ -8,52 +8,54 @@ from matplotlib import style
 
 df=quandl.get('WIKI/GOOGL')
 style.use('ggplot')
-df=df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+df=df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT']=(df['Adj. High']-df['Adj. Close'])/df['Adj. Close']*100
 df['PCT_change']=(df['Adj. Close']-df['Adj. Open'])/df['Adj. Open']*100
 
 df=df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
 
 forecast_col='Adj. Close'
-df.fillna(-99999,inplace=True) #remove Nan by -99999 bcz we can't proceed without a number i.e. with a Nan
+df.fillna(value=-99999,inplace=True) #remove Nan by -99999 bcz we can't proceed without a number i.e. with a Nan
 
-forcast_out=math.ceil(0.01*len(df))
-forcast_out=int(forcast_out)
-print(forcast_out)
+forecast_out=math.ceil(0.01*len(df))
+forecast_out=int(forecast_out)
+print(forecast_out)
 
-df['label']=df[forecast_col].shift(-forcast_out)
-#df.dropna(inplace=True)
+df['label']=df[forecast_col].shift(-forecast_out)
 x=np.array(df.drop(['label'],1))
-x=x[:-forcast_out]
-x_lately=x[-forcast_out:]
 x=preprocessing.scale(x)
+x_lately=x[-forecast_out:]
+x=x[:-forecast_out]
 df.dropna(inplace=True)
 y=np.array(df['label'])
-y=np.array(df['label'])
+#y=np.array(df['label'])
 x_train,x_test,y_train,y_test=model_selection.train_test_split(x,y,test_size=0.2)
 clf=LinearRegression(n_jobs=-1)
 clf.fit(x_train,y_train)
 acc=clf.score(x_test,y_test)
 
 forecast_set=clf.predict(x_lately)
-print(forecast_set,acc)
+#print(forecast_set)
 
-df['forecast']=np.nan
+df['Forecast']=np.nan
 
 ldate=df.iloc[-1].name
 
 last_unix=ldate.timestamp()
 
-oday=86365
+oday=86400
 
 nunix=last_unix+oday
 
 for i in forecast_set:
 	ndate=datetime.datetime.fromtimestamp(nunix)
+	nunix+=86400
 	df.loc[ndate]=[np.nan for _ in range (len(df.columns)-1)]+[i]
 
+print(df.head())
+
 df['Adj. Close'].plot()
-df['forecast'].plot()
+df['Forecast'].plot()
 plt.legend(loc=4)
 plt.xlabel('Date')
 plt.ylabel('Price')
