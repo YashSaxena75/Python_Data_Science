3d2d7f4ae84a1b2b7ed0be805c72d6eeef2439ea
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2019-07-08 23:47:03 +0530

    Update linear_reg.py

--- linear_reg.py
+++ linear_reg.py
@@ -7,7 +7,7 @@ import quandl,math,pickle
 from matplotlib import style
 
 df=quandl.get('WIKI/GOOGL',authtoken="s4szyQkyTGQP9pmDuTEu")
-style.use('ggplot')
+style.use('dark_background')
 df=df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT']=(df['Adj. High']-df['Adj. Close'])/df['Adj. Close']*100
 df['PCT_change']=(df['Adj. Close']-df['Adj. Open'])/df['Adj. Open']*100
